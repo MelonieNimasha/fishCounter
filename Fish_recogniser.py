389be940434a7935bf82eb419a5e32fca3facfc3
MelonieNimasha <36945783+MelonieNimasha@users.noreply.github.com>
2018-11-05 17:31:36 +0530

    Update Fish_recogniser.py

--- Fish_recogniser.py
+++ Fish_recogniser.py
@@ -1,17 +1,17 @@
 import numpy as np
-import DeepLearning
+import DeepLearning as DL
 
 def trainer(N,X_data,Y_data,alpha,number):
 	for num in range number:
-		W,b= initialize(N)
-		Z,A= forward_prop(W,b,X_data,N)
-		Cost=Cost_function(Y_caps,Y_data)
+		W,b= DL.initialize(N)
+		Z,A= DL.forward_prop(W,b,X_data,N)
+		Cost=DL.Cost_function(Y_caps,Y_data)
 		print(Cost)
-		dW, db= back_propagation(N,Z,A,W,Y_data)
-		W,b=gradient_descent(W,b,dW, db,alpha)
+		dW, db= DL.back_propagation(N,Z,A,W,Y_data)
+		W,b=DL.gradient_descent(W,b,dW, db,alpha)
 	return W,b
 
 def predictor(X,W,b):
-	Z,A=forward_prop(W,b,X_data,N)
+	Z,A=DL.forward_prop(W,b,X_data,N)
 	return A[str(len(N)-1)]
 
