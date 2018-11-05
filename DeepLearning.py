215a98e44d1906d4bf22ba29a6c5e1784d546b06
MelonieNimasha <36945783+MelonieNimasha@users.noreply.github.com>
2018-11-05 17:30:12 +0530

    Update DeepLearning.py

--- DeepLearning.py
+++ DeepLearning.py
@@ -1,5 +1,4 @@
 import numpy as np
-import ML
 
 def initialize(N): #N= the list of number of hidden units in each layer
 	W={}
@@ -15,7 +14,7 @@ def sigmoid(z):            #sigmoid activation  function
 	return (1/(1+np.exp(z)))
 
 def sigmoid_dash(Z):
-	return(ML.sigmoid(Z)*(1-ML.sigmoid(Z)))
+	return(sigmoid(Z)*(1-ML.sigmoid(Z)))
 
 def relu(z):
 	z_abs=np.abs(z)    #relu activation function 
@@ -32,9 +31,9 @@ def forward_prop(W,b,X_data,N):
 	for l in range(L-1):
 		Z[str(l+1)]=np.matmul(W[str(l+1)],A[str(l)])+b[str(l+1)] #Compute Z/cache
 		if l<(L-2):
-			A[str(l+1)]=ML.relu(Z[str(l+1)]) #Compute activation for hidden layers
+			A[str(l+1)]=relu(Z[str(l+1)]) #Compute activation for hidden layers
 		else:
-			A[str(l+1)]=ML.sigmoid(Z[str(l+1)]) #Compute activation for outmost layer
+			A[str(l+1)]=sigmoid(Z[str(l+1)]) #Compute activation for outmost layer
 	return Z,A
 
 def Cost_function(Y_caps,Y_data):
